91e8606797ca0bd1701b6d366f1ea9ff719cc3ab
chajancode <chajancode@gmail.com>
2026-03-04 23:46:21 +0400

    добавлены паттерны для имейлов и телефонных номеров

--- utils/regexp_patterns.py
+++ utils/regexp_patterns.py
@@ -5,14 +5,21 @@ class RegexpPatterns:
 
     PHONE_PATTERN = re.compile(
         r'^\+(\d{1,3})'
-        r'[-.\s]?(\d{1,4})'
-        r'[-.\s]?(\d{1,4})'
-        r'[-.\s]?(\d{1,9})$'
+        r'[\s.-]*'
+        r'(\d{4,5})'
+        r'[\s.-]+'
+        r'(\d{2,3})'
+        r'[\s.-]+'
+        r'(\d{3,4})$'
     )
     SKYPE_PATTERN = re.compile(r'^skype:[^?]*\?chat$')
-    EMAIL_PATTERN = re.compile(
+    EMAIL_LINK_PATTERN = re.compile(
         r'^mailto:[a-zA-Z0-9._%+-]+'
         r'@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    )
+    EMAIL_PATTERN = re.compile(
+        r'^[a-zA-Z0-9._-]'
+        r'+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}$'
     )
     SOCIAL_MEDIA_PATTERN = re.compile(
         r'.*(google\.com|linkedin\.com|facebook\.com|youtube\.com).*'
