b6b6109149fa632e7bc88faabb51be14c4959243
chajancode <chajancode@gmail.com>
2026-04-27 23:07:35 +0400

    Update Selenoid configuration: change command executor URL and add SELENOID_URL environment variable

--- config/drivers.py
+++ config/drivers.py
@@ -4,7 +4,7 @@ from selenium.webdriver import Chrome, Firefox, Edge
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium import webdriver
 
-from config.params import URL_GRID
+# from config.params import URL_GRID
 from config.browser_options import (
     get_chrome_options,
     get_edge_options,
@@ -53,7 +53,7 @@ class DriverFactory:
 
         if grid_mode:
             driver = webdriver.Remote(
-                command_executor='http://localhost:4444/wd/hub',
+                command_executor='http://selenoid:4444/wd/hub',
                 options=options
             )
         else:
