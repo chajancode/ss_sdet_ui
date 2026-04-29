702c3613987b955e80289253b3e8fe3f6fe956de
chajancode <chajancode@gmail.com>
2026-04-29 15:37:20 +0400

    zarabotalo

--- bash_scripts/selenoid_run.sh
+++ bash_scripts/selenoid_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pytest -n 3 --dist=loadscope --reruns 2 --alluredir=./allure-results --grid tests/test_js_sqlex.py
+pytest -n 3 --dist=loadscope --reruns 2 --alluredir=./allure-results --grid tests/test_main_page.py
