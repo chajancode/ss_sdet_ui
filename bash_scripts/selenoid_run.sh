f63cba570aeaf4c215ced70ac93e3d11b86db92e
chajancode <chajancode@gmail.com>
2026-07-11 23:04:54 +0400

    уменьшить количество потоков в selenoid_run.sh с 3 до 2

--- bash_scripts/selenoid_run.sh
+++ bash_scripts/selenoid_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # bash_scripts/selenoid_run.sh
-pytest -n 3 --dist=loadscope --reruns 2 --alluredir=./allure-results --grid --browser chrome,firefox
+pytest -n 2 --dist=loadscope --reruns 2 --alluredir=./allure-results --grid --browser chrome,firefox
